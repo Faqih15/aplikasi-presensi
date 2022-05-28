1a463a714d5f8005fd62e740051d7aec4bcb9ca7
Faqih15 <jahidiyam1592@gmail.com>
2022-05-28 10:46:31 +0700

    fungsi 1

--- src/pages/Gantiwarna.js
+++ src/pages/Gantiwarna.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Gantiwarna() {
-    const [warna, setwarna] = useState(second)
-    
+    const warna = ["bg-purple-600", "bg-lime-500", "bg-red-600", "bg-blue-600"]
+    const [index, setIndex] = useState(0)
+
+    const fungsiganti = () => {
+        if (index === 3) {
+            setIndex(0)
+        } else {
+            setIndex(index + 1)
+        }
+    }
     return (
-        <div className='text-[#25db25] text-[8rem] font-[800]'>Gantiwarna</div>
+        <div >
+            <div className={`${warna[index]} w-[8rem] h-[8rem]`}></div>
+            <button onClick={fungsiganti} className={`bg-[#f30808]`}>Ganti Warna</button>
+        </div>
     )
 }
 
