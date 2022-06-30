24c1572dc3f42d248fd6724b4a91bc74f0778228
Faqih15 <jahidiyam1592@gmail.com>
2022-06-30 11:10:45 +0700

    presensi sudah jadi

--- src/presensi/Hadir.js
+++ src/presensi/Hadir.js
@@ -1,79 +1,80 @@
 import React, { useEffect, useState } from 'react'
+import * as dayjs from 'dayjs'
+
+const defaultAbsen = [
+  { nama: 'agus', tanggal: '24-06-2022', masuk: '09:00:00', keluar: '15:00:00' },
+]
 
 function Hadir() {
-  const [login, setlogin] = useState([])
 
-  const [list, setList] = useState(JSON.parse(localStorage.getItem("pendaftaran")) || [])
+  const localSiswa = localStorage.getItem("siswa")
+  const localAbsen = localStorage.getItem("absen")
+  const [siswa] = useState(JSON.parse(localSiswa) || [])
+  const [absen, setAbsen] = useState(JSON.parse(localAbsen) || [])-
 
-  useEffect(() => {
-    if (localStorage.getItem("pendaftaran")) {
-      setList(JSON.parse(localStorage.getItem("pendaftaran")))
-    }
-  }, [])
-
-  const ndaftar = (event) => {
+  const login = (event) => {
     event.preventDefault()
     const nama = event.target.name.value
-    const email = event.target.email.value
-    const pass = event.target.password.value
+    const siswaLogin = siswa.find(x => x.nama === nama)
+    const tanggal = dayjs().format('DD:MM:YYYY');
+    const masuk = dayjs().format('HH:mm:ss')
 
-    if (nama && email && pass) {
-      const databaru = { nama: nama, email: email, password: pass }
-      const data = [...login]
-      data.push(databaru)
-      // console.log("ini adalah data push", data);
-      setList(data)
-      localStorage.setItem("pendaftaran", JSON.stringify(data))
-      setlogin(data)
-      // alert("berhasil")
-      event.target.nama.value = ""
-      event.target.email.value = ""
-      event.target.password.value = ""
-      console.log("SATU 1");
+    if (siswaLogin) {
+      const siswaAbsen = absen.find(x => x.nama === nama && x.tanggal === tanggal)
+      console.log("0");
+      if (siswaAbsen) {
+        const newAbsen = [...absen]
+        console.log("1")
+        newAbsen.push({ nama, tanggal, masuk, keluar: '' })
+        setAbsen(newAbsen)
+        localStorage.setItem('absen', JSON.stringify(newAbsen))
+      } else {
+        console.log("2")
+        const newAbsen = [...absen]
+        newAbsen.push({ nama, tanggal, masuk, keluar: '' })
+        setAbsen(newAbsen)
+        localStorage.setItem('absen', JSON.stringify(newAbsen))
+      }
+    } else {
+      alert("daftar dulu dong baru login")
+      console.log("3");
+    }
+    event.target.name.value = ""
+  }
+  const handleKeluar = (e, a, index) => {
 
-    } else { alert("GAGAL") }
+    // e.preventDefault()
+    // console.log(a, b)
 
+    const siswaAbsen = absen.find(x => x.nama === a.nama && x.tanggal === a.tanggal)
+    // console.log(siswaAbsen);
+    const keluar = dayjs().format('HH:mm:ss')
+    if (siswaAbsen) {
+      console.log("siswa absen ada");
+      const newAbsen = [...absen]
+      newAbsen[index].keluar = keluar
+      // newAbsen.keluar === "keluar" ? { ...absen, keluar } : newAbsen
+      console.log(newAbsen);
+      setAbsen(newAbsen)
+      localStorage.setItem('absen', JSON.stringify(newAbsen))
+    }
   }
 
-  const jamMelbu = (event) => {
-    event.preventDefault();
-    // const now = new Date()
-    // const jamMasuk = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
-    // const nama = event.target.label.value
-    // const listBaru = { nama: nama, jamMasuk: jamMasuk, jamKeluar: "" }
-    // const tambahkan = list.concat(listBaru)
-    // localStorage.setItem("data harian", JSON.stringify(tambahkan))
-    // setList(tambahkan)
-    // event.target.label.value = "" //untuk mengosongkan input setelah submit
-  }
-
-  const jamMetu = (e, index) => {
-    e.preventDefault()
-    // const now = new Date()
-    // const jamKeluar = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
-    // const newList = [...list]
-    // newList[index].jamKeluar = (jamKeluar)
-    // localStorage.setIstem("data harian", JSON.stringify(newList))
-    // setList(newList)
-
-  }
-
-  console.log(list, 'list');
+  // useEffect(() => {
+  //   console.log("isi dari localStorage absen :", absen)
+  // }, [absen])
 
   return (
-    <div className='py-2 px-4'>
+    <div className='p-2 m-2 md:py-2 md:px-4 border-red-600 border-4'>
       <div>
         <p className='py-2 px-2 text-black font-extrabold text-[1,5rem]'>Login Karyawan PT Bajaj Bajuri</p >
       </div>
 
-      <form className='text-2xl my-2 mx-2'
-        onSubmit={ndaftar}
-      >
+      <form className='text-2xl my-2 mx-2' onSubmit={login} >
         <label className="block">
-
           <div className='py-2'>
             <span
-              className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xl font-medium 
+              className="after:content-['*'] after:ml-0.5 after:text-red-500 block font-medium 
                              text-black text-lg">
               Name
             </span>
@@ -84,12 +85,9 @@ function Hadir() {
                              block w-full rounded-md sm:text-sm focus:ring-1"
               placeholder="input your name" autoComplete='off' />
           </div>
-
-
         </label>
-
         <div className='mr-4'>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 text-xl my-4 
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 text-xl my-4 
                             border border-blue-700 rounded"
             type="submit">
             Login
@@ -103,57 +101,62 @@ function Hadir() {
           <p className='py-2 px-2 text-black font-extrabold text-[1rem]'>
             Presensi Karyawan</p >
         </div>
-        {list.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className="justify-between items-center font-bold grid grid-cols-4 mx-6">
-              <p>{item.nama}</p>
-              <p>{item.jamMasuk}</p>
-              <button key={index}
-                className="bg-[#0f0cd8] px-1 py-2 m-5"
-              // onClick={(e) => { jamMetu(e, index) }} 
-              >
-                Keluar
-              </button>
-              <p>{item.jamKeluar}</p>
+        {absen.map((x, i) => {
+          return <div key={i} className='grid grid-cols-2'>
+            <div className='grid grid-cols-1'>
+              <span className=''>nama</span>
+              <span>tanggal</span>
+              <span>jam masuk</span>
+              <span>jam keluar</span>
             </div>
-          )
+            <div className='grid grid-cols-1'>
+              <span className=''>{x.nama}</span>
+              <span>{x.tanggal}</span>
+              <span>{x.masuk}</span>
+              <span>{x.keluar}</span>
+            </div>
+            <button className='' onClick={(e) => { handleKeluar(e, x, i) }}>Keluar</button>
+          </div>
         })}
       </div>
-
+      
     </div>
   )
 
 
 }
-
+// event adalah fungsi callback yang mempunyai return/parameter object 
 export default Hadir
 
-{/* <div>
-<span
-  className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xl font-medium 
-                 text-black text-lg">
-  Email
-</span>
-<input
-  type="email" name="email"
-  className="mt-1 px-3 py-2 bg-white border shadow-sm border-[#020202]
-                 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 
-                 block w-full rounded-md sm:text-sm focus:ring-1"
-  placeholder="contoh@example.com" autoComplete='off' />
-</div>
+  // const login = (event) => {
+  //   event.preventDefault()
+  //   const datadaftar = JSON.parse(localStorage.getItem("dataPendaftaran"))
+  //   const inputed = event.target.name.value
+  //   const today = new Date();
+  //   const yyyy = today.getFullYear();
+  //   const mm = today.getMonth() + 1;
+  //   const dd = today.getDate();
+  //   const tanggal = dd + '-' + mm + '-' + yyyy;
+  //   // const presensi = JSON.parse(localStorage.getItem("presensi"))
+  //   const temukan = datadaftar.find((i) => i.nama === inputed);
+  //   // console.log("temukan 1", temukan);
 
-<div className='py-2'>
-<span
-  className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-xl font-medium 
-                 text-black text-lg">
-  Password
-</span>
-<input
-  type="text" name="password"
-  className="mt-1 px-3 py-2 bg-white border shadow-sm border-[#020202]
-                 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 
-                 block w-full rounded-md sm:text-sm focus:ring-1"
-  placeholder="input your password" autoComplete='off' />
-</div> */}
+  //   if ((inputed === temukan.nama || temukan) && localStorage.getItem(tanggal) === null) {
+  //     console.log(inputed, "kondisi tanggal blm ada")
+  //     localStorage.setItem("dataPresensi", JSON.stringify([{ nama: inputed, jamMasuk: "", jamKeluar: "" }]))
+  //     localStorage.setItem("dataabsen", JSON.stringify(temukan))
+  //     setloginer(temukan)
+  //     setAbsen(temukan)
+  //   } else if ((inputed === temukan.nama || temukan) && localStorage.hasOwnProperty(tanggal)) {
+  //     console.log("else if jalan, update key local storage tanggal")
+  //     const data1 = JSON.parse(localStorage.getItem(tanggal));
+  //     const data2 = [{ nama: inputed, jamMasuk: "", jamKeluar: "" }];
+  //     data1.push(data2);
+  //     localStorage.setItem(tanggal, JSON.stringify(data1));
+  //     setloginer(temukan)
+  //   } else if (inputed !== temukan.nama || temukan) {
+  //     console.log("else if terakhir jalan")
+  //     alert("GAGAL")
+  //   }
+  //   event.target.name.value = ""
+  // }
